2dff804bced25248af9b502b07e41019771a5cf1
jacksonhuish88 <jacksonhuish88@gmail.com>
2023-03-31 18:12:05 -0600

    mission13

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MyHeader from "./myHeader";
+import MovieList from "./Movies";
+import Home from "./Home";
+import Podcasts from "./Podcasts";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <MyHeader />
+        <div style={{ paddingTop: "25px" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Podcasts" element={<Podcasts />} />
+            <Route path="/MovieCollection" element={<MovieList />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+// function GoButton(props: any) {
+//   return (
+//     <div>
+//       <button onClick={props.incrementGo}>Go!</button>
+//       <br></br>
+//       <label>{props.count}</label>
+//     </div>
+//   );
+// }
+
+// function AmountOfGo(props: any) {
+//   return <label>{props.count}</label>;
+// }
